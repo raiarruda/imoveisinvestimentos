9428f3ef36f8e359eb47c82c37a91bc4c721a1f7
Raissa <raiarruda.raa@gmail.com>
2019-05-23 12:55:43 -0300

    add table img

--- views/teste-upload-imagens.php
+++ views/teste-upload-imagens.php
@@ -1,8 +1,45 @@
 
-<h1>Upload de Arquivos</h1>
-<?php if(isset($msg) && $msg != false) echo "<p> $msg </p>"; ?>
-<form action="upload.php" method="POST" enctype="multipart/form-data">
-  Arquivo: <input type="file" required name="arquivo">
-  <input type="submit" value="Salvar">
+<h1>Upload de  oi  Arquivos</h1>
+<!-- if(isset($msg) && $msg != false) echo "<p> $msg </p>";  -->
+<!-- <form action="upload.php" method="POST" enctype="multipart/form-data" >
+  Arquivo: <input type="file" required name="files[]" multiple>
+  <input type="submit" name='submit' value="Salvar">
   
+</form> -->
+
+
+<!-- images upload form -->
+<form method="post" id="uploadForm" enctype="multipart/form-data" action="upload.php">
+    <label>Choose Images</label>
+    <input type="file" name="images[]" id="images" multiple >
+    <input type="submit" name="submit" value="UPLOAD"/>
 </form>
+
+<!-- display upload status -->
+<div id="uploadStatus"></div>
+
+<!-- gallery view of uploaded images --> 
+<div class="gallery" id="imagesPreview"></div>
+
+
+
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
+
+<script>
+$(document).ready(function(){
+    $('#uploadForm').ajaxForm({
+        target:'#imagesPreview',
+        beforeSubmit:function(){
+            $('#uploadStatus').html('<img src="uploading.gif"/>');
+        },
+        success:function(){
+            $('#images').val('');
+            $('#uploadStatus').html('');
+        },
+        error:function(){
+            $('#uploadStatus').html('Images upload failed, please try again.');
+        }
+    });
+});
+</script>
--- views/upload.php
+++ views/upload.php
@@ -1,28 +1,65 @@
 <?php
+  
   include("../models/conecta.php");
 
-  $msg = false;  //avisar se deu erro
 
-  if(isset($_FILES['arquivo'])){
-   
-    $img = $_FILES['arquivo']['name'];
-    // $extensao = strtolower(substr($_FILES['arquivo']['name'], -4)); //pega a extensao do arquivo
-    // $novo_nome = md5(time()) . $extensao; //define o nome do arquivo
-   
-    $final = explode( ".", $_FILES['arquivo'] ["name"] );
-    $extensao = strtolower( end( $final ) ); // o end só recebe variavel como paramentro, por isso, a quebra
-    $novo_nome = md5(time()). "." . $extensao;
-    $diretorio = "../imgs/"; //define o diretorio para onde enviaremos o arquivo
-    $string =  $diretorio . $novo_nome;
-   
-    move_uploaded_file($_FILES['arquivo']['tmp_name'], $string); //efetua o upload
-   
-    $sql_code = "INSERT INTO img_imovel (id, nome_arquivo, data) VALUES(null, '$novo_nome', NOW())";
-   
-    if(mysqli_query($conexao, $sql_code))
-      $msg = "Arquivo enviado com sucesso!";
-    else
-      $msg = "Falha ao enviar arquivo.";
 
-    echo $msg;
-  }
+if(isset($_POST['submit'])){
+    // File upload configuration
+    $targetDir = "../imgs/";
+    $allowTypes = array('jpg','png','jpeg','gif');
+    
+    $images_arr = array();
+    foreach($_FILES['images']['name'] as $key=>$val){
+        $image_name = $_FILES['images']['name'][$key];
+        $tmp_name   = $_FILES['images']['tmp_name'][$key];
+        $size       = $_FILES['images']['size'][$key];
+        $type       = $_FILES['images']['type'][$key];
+        $error      = $_FILES['images']['error'][$key];
+        
+        // File upload path
+        $fileName = basename($_FILES['images']['name'][$key]);
+        $targetFilePath = $targetDir . $fileName;
+        
+        // Check whether file type is valid
+        $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+        if(in_array($fileType, $allowTypes)){    
+            // Store images on the server
+            if(move_uploaded_file($_FILES['images']['tmp_name'][$key],$targetFilePath)){
+                $images_arr[] = $targetFilePath;
+            }
+        }
+    }
+    
+    // Generate gallery view of the images
+    if(!empty($images_arr)){ ?>
+        <ul>
+        <?php foreach($images_arr as $image_src){ ?>
+            <li><img src="<?php echo $image_src; ?>" alt=""></li>
+        <?php } ?>
+        </ul>
+<?php }
+}// if(isset($_FILES['arquivo'])){
+  //   $img = $_FILES['arquivo']['name'];
+  //   // $extensao = strtolower(substr($_FILES['arquivo']['name'], -4)); //pega a extensao do arquivo
+  //   // $novo_nome = md5(time()) . $extensao; //define o nome do arquivo
+   
+  //   $final = explode( ".", $_FILES['arquivo'] ["name"] );
+  //   $extensao = strtolower( end( $final ) ); // o end só recebe variavel como paramentro, por isso, a quebra
+  //   $novo_nome = md5(time()). "." . $extensao;
+  //   $diretorio = "../imgs/"; //define o diretorio para onde enviaremos o arquivo
+  //   $string =  $diretorio . $novo_nome;
+   
+  //   move_uploaded_file($_FILES['arquivo']['tmp_name'], $string); //efetua o upload
+   
+  //   $sql_code = "INSERT INTO img_imovel (id, nome_arquivo, data) VALUES(null, '$novo_nome', NOW())";
+   
+  //   if(mysqli_query($conexao, $sql_code))
+  //     $msg = "Arquivo enviado com sucesso!";
+  //   else
+  //     $msg = "Falha ao enviar arquivo.";
+
+  //   echo $msg;
+  // }
+?>
+  
