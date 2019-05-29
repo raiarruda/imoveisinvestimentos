55b2c94a520808bbc3117012acd3a246ea2c9f5e
thiago <thiago.nam@gmail.com>
2019-05-29 20:44:15 -0300

    alteração do cabeçalho e e adição do JS do bootstrap  no rodapé

--- views/cabecalho.php
+++ views/cabecalho.php
@@ -2,14 +2,45 @@
 <head>
     <title>Imoveis Investimentos</title>
     <meta charset="utf-8">
-    <link href="css/bootstrap.css" rel="stylesheet" />
-    <link href="css/loja.css" rel="stylesheet" />
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/site.css" rel="stylesheet" />
 </head>
 
 <body>
-    <div class="container">
-    <a href="/imoveisinvestimentos/index.php">HOME</a>
-        <div class="principal">
+    <header>
+        <div class="container">
+
+        <!-- https://via.placeholder.com/125x125?text=125x125+Square+Button -->
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a href="index.php"><img src="https://via.placeholder.com/125x125?text=125x125+Square+Button"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Quem Somos <span class="sr-only">(Página atual)</span></a>
+                        </li>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Preços</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="views/lista-loja.php" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            LOJA
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="views/lista-loja.php">Lista da Loja</a>
+                            <a class="dropdown-item" href="views/form-loja.php">ADD Loja</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+            <a href="/imoveisinvestimentos/index.php">HOME</a>
+         <div class="principal">
     <!-- fim cabecalho.php -->
     <!-- TODO: atualizar com o codigo de teste/indez.html -->
 
