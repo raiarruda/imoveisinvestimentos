1b5c04b56ea4180f04e30a7094d8bcd9fb7efc7f
thiago <thiago.nam@gmail.com>
2019-06-02 18:15:54 -0300

    alteração do form-loja.php

--- views/form-loja.php
+++ views/form-loja.php
@@ -12,36 +12,62 @@
      </div>
 
     <div class="form-group row">
-        <label class="col-sm-1 col-form-label" for="area">Area</label>
+        <label class="col-sm-1 col-form-label" for="area">Area:</label>
         <div class="col-sm-11">
             <input  type="number" step="0.05" min="0"  class="form-control" placeholder="Exemplo: 25.6 m² " name="area">
         </div>
      </div>
 
+    <!-- Abrir Características -->
     <div class="form-group row">
-        <div class="col-sm-2">Caracteristicas</div>
-            <div class="col-sm-12">
-                <div class="form-check">
-                <input class="form-check-input"  name="tem_mezzanino"  value="true" type="checkbox" id="mezzanino">
-                <label class="form-check-label" for="mezzanino">Tem mezzanino</label>
-            </div>
+      <div class="col-md-auto">Características:</div>
+        <div class="col-sm-9">
+
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="mezzanino">
+              <label class="form-check-label" for="mezzanino">
+                Mezzanino
+              </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="shopping">
+              <label class="form-check-label" for="shopping">
+                Shopping
+              </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="centro_comercial">
+              <label class="form-check-label" for="centro_comercial">
+                Centro Comercial
+              </label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="frente_de_rua">
+              <label class="form-check-label" for="frente_de_rua">
+                Frente de Rua
+              </label>
+          </div>
+          
         </div>
     </div>
-    Shopping
-    <input class="form-control" type="number" name="shopping" />
+    <!-- Fim das Características -->
 
-    Centro Comercial
-    <input class="form-control" type="number" name="centro_comercial" />
-    
-    Frente de rua<input class="form-control" type="number" name="frente_rua" />
-
-    <!-- TODO selected -->
-    Pavimentacao<input class="form-control" type="number" name="tipo_de_rua" />
-
+    <!-- Inicio da Pavimentação --> 
+    <div class="form-group row">
+      <label class="col-sm-auto col-form-label" for="pavimentacao">Pavimentação:</label>
+        <div class="col-lg-2">
+          <select class="form-control" id="pavimentacao">
+            <option>Asfalto</option>
+            <option>Pedra Portuguesa </option>
+            <option>Bloquete</option>
+          </select>
+        </div>
+    </div>
+    <!-- Fim da Pavimentação -->
 
     <div class="form-group row">
-        <label class="col-sm-1 col-form-label" for="localizacao">Localização</label>
-        <div class="col-sm-11">
+        <label class="col-md-auto col-form-label" for="localizacao">Localização:</label>
+        <div class="col-sm-8">
             <input type="text"  class="form-control" placeholder="Digite o endereço" name="localizacao">
         </div>
      </div>
@@ -55,7 +81,6 @@
      </div>
 
 
-     <label for="story">Tell us your story:</label>
 
     
     <button class="btn btn-primary" type="submit">Cadastrar</button>
@@ -64,30 +89,3 @@
 
 
 <?php include("rodape.php"); ?>
-<form>
-  
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>
